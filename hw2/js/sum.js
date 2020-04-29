ce0bfdd6ff4885fcc49606735eea86dba00fcb2d
Henock W <46727864+henyoha19@users.noreply.github.com>
2020-04-28 21:35:50 -0700

    Add files via upload

---
+++ hw2/js/sum.js
@@ -0,0 +1,10 @@
+var name = prompt("Let's play the sum game." + "\r\n" + "Please enter your Name");
+var num1 = parseFloat(prompt("Enter first number"));
+var num2 = parseFloat(prompt("Enter second number"));
+var total = num1 + num2;
+
+document.write("Hi," + "   " + name + "!" + "   " + "The sum of" + "   " + num1 + "  " + "+" + "  " + num2 + "  " + "is" + "  " + total);
+
+
+
+
